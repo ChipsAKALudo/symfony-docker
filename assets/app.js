aa65aec8c28496d954f93cdb5a12e81f00dd24d6
Ludovic <115978451+ChipsAKALudo@users.noreply.github.com>
2024-09-24 20:45:13 +0200

    react working

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,6 @@
 // assets/app.js
-import { registerReactControllerComponents } from '@symfony/ux-react';
+import {registerReactControllerComponents} from '@symfony/ux-react';
+import './bootstrap';
 
+console.log(registerReactControllerComponents);
 registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
